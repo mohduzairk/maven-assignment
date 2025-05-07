79e24acd511e1fbdb485bb21846e0b3766456a75
Mohd Uzair Khan <mohduzairkhan99@gmail.com>
2025-05-08 04:23:38 +0530

    initial commit

---
+++ src/main/java/com/edureka/maven_assignment/MavenAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.edureka.maven_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenAssignmentApplication.class, args);
+	}
+
+}
